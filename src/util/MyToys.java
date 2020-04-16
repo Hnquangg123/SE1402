2450feef000b2c72e9c382e90040cca102455d8d
Hnquangg123 <rutickdeson@gmail.com>
2020-04-16 23:04:06 +0700

    2nd push: TDD package with JUnit

--- src/util/MyToys.java
+++ src/util/MyToys.java
@@ -32,5 +32,18 @@ public class MyToys {
         }
 
     }
-
+    
+    // moi doan code ban viet ra phai dam bao co chat luong
+    // chat luong nghia la: ham tra ve, xu li dung nhu minh du kien
+    // expected
+    // vi du tinh giai thua() thi ta dua vao 5, hy vong nhan ve 120
+    // vay ta phai chuan bi bo du lieu de test tung ham, dam bao no xu li dung
+    // tung ham xu li dung, class xu li dung
+    // viec dam bao cho tung class/module/tung don the/tung ham chay nhu
+    // du kien -> DEV co trach nhiem vu nay, viec nay song song viet code
+    // qua trinh nay goi la UNIT TESTING
+    // lam sao test ham() chay dung ko
+    // chuan bi data -> dua data -> goi ham -> nhin ket qua -> so voi du kien
+    // nhin ket qua: mat 
+    
 }
---
+++ test/tdd/MyToyTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static util.MyToys.cF; // lat hoi xai ham static eo can MyToys.
+                              // goi cF() nhu ben C
+
+/**
+ *
+ * @author Dell
+ */
+public class MyToyTest {
+    
+    // trong day se la nhung tinh huong minh ham can test (test cases)
+    // vi du ta test tinh huong dua vao ngon lanh, nhan ve ngon lanh
+    
+    // vi du ta test tinh huong dua vao ca chon, nhan ve Exception
+    // dam bao nhung tinh huong nay chay nhu du kien/mong doi/expected
+    // luc do NetBeans nem mau xanh khi on, do khi co van de
+    @org.junit.Test
+    public void testSuccessfullCases() {
+        
+        assertEquals(1, cF(0));
+        assertEquals(6, cF(1));
+        assertEquals(6, cF(2));
+        assertEquals(24, cF(3));
+        assertEquals(120, cF(4));
+        assertEquals(720, cF(5));
+        assertEquals(820, cF(6));   // do? xay ra di 2 tinh huong
+                                    // hoac ban xu li sai, hoac ban ki 
+                                    // vong sai
+                                    // co do?, kiem tra code di
+    }
+    
+    
+    // Ngoai le Exception la 1 thu ko the là value đe uoc luong
+    // vay thi ko the assert đc, vi no can phai co value de so sanh 
+    // xai them ki thuat bat ngoai le, coi ngoai le xay ra giong
+    // nhu minh ki vong
+    
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void testExceptionCases() {
+        
+        // ko co value de assert do ham cF() nem ra ngoai le
+        cF(-5);
+        cF(100);        
+    }
+    
+    
+    // CODE KO BI LOI~ CU PHAP, THAM CHIEU THU VIEN, LUON CLEAN AND BUILD RA FILE JAR
+    // TEST DUNG MAT, MAU, CAU CHUYEN LOGIC TRONG CODE, CHU KO PHAI LA CAU CHUYEN CUA BUILD JAR
+    // BUILD JAR, .JAVA -> .CLASS, NEN .CLASS .CLASS -> .JAR  .WAR (.RAR)
+    // TIEM AN RUI RO LON, CODE SAI LOGIC, KO SAI CU PHAP
+    // NANG CAO, ME BO, NEU SAI LOGIC, DANG MAU DO - EO CHO RA .JAR
+    // JUNIT DEU KET HOP DC VOI ANT DE CAM APP KO
+    
+}
